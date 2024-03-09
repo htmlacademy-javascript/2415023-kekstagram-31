c1b889c82d0f7ace037ee1e7902c3b538dbfa935
Anastasia <an.gorskova.bg@gmail.com>
2024-03-09 04:21:35 +0100

    Создает массив комментариев

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 // Объявляем переменные
 // Посты
-const POSTS_NUMBER = 5;
+const POSTS_NUMBER = 25;
 
 const PHOTO_ID = {
   min: 1,
@@ -42,32 +42,32 @@ const LIKES_NUMBER = {
 
 
 // Комменты
-// const COMMENTS_NUMBER = {
-//   min: 0,
-//   max: 30
-// };
+const COMMENTS_NUMBER = {
+  min: 0,
+  max: 30
+};
 
-// const USER_NAMES = [
-//   'Максим',
-//   'Алина',
-//   'Павел',
-//   'Анастасия',
-//   'Даниил',
-//   'Наталья',
-//   'Иван',
-//   'Полина',
-//   'Артем',
-//   'Ольга'
-// ];
+const USER_NAMES = [
+  'Максим',
+  'Алина',
+  'Павел',
+  'Анастасия',
+  'Даниил',
+  'Наталья',
+  'Иван',
+  'Полина',
+  'Артем',
+  'Ольга'
+];
 
-// const COMMENT_MESSAGES = [
-//   'Всё отлично!',
-//   'В целом всё неплохо. Но не всё.',
-//   'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
-//   'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
-//   'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
-//   'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
-// ];
+const COMMENT_MESSAGES = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+];
 
 
 // Основная часть кода
@@ -80,30 +80,41 @@ function getRandomInteger (min, max) {
   return Math.floor(result);
 }
 
-// Создаем комменты
 
-// Создаем посты
-const usedId = [];
+// Создание постов
+const postUsedId = [];
+
 const createPost = () => {
-  const randomId = getRandomInteger(PHOTO_ID.min, PHOTO_ID.max);
-  const randomLikesNumber = getRandomInteger(LIKES_NUMBER.min, LIKES_NUMBER.max - 1);
+  const randomPostId = getRandomInteger(PHOTO_ID.min, PHOTO_ID.max);
+  const randomPostLikesNumber = getRandomInteger(LIKES_NUMBER.min, LIKES_NUMBER.max - 1);
 
-  if (!usedId.includes(randomId)) {
-    usedId.push(randomId);
+  // Создаем комменты
+
+  const createComment = () => ({
+    avatar: `img/avatar-${getRandomInteger(1, 6)}.svg`,
+    message: COMMENT_MESSAGES[getRandomInteger(0, COMMENT_MESSAGES.length - 1)],
+    name: USER_NAMES[getRandomInteger(0, USER_NAMES.length - 1)]
+  });
+
+  // Создаем массив комментов
+  const commentsList = [];
+  const commentsNumber = getRandomInteger(COMMENTS_NUMBER.min, COMMENTS_NUMBER.max);
+  for (let i = 1; i <= commentsNumber; i++) {
+    const postComments = createComment();
+    postComments.id = randomPostId.toString() + (i - 1).toString();
+    commentsList.push(postComments);
+  }
+
+  // Создаем пост
+  if (!postUsedId.includes(randomPostId)) {
+    postUsedId.push(randomPostId);
 
     return {
-      id: randomId,
-      url: `photos/${[randomId]}.jpg`,
-      description: DESCRIPTIONS[randomId - 1],
-      likes: randomLikesNumber,
-    //   comments: [
-    //     const COMMENT = {
-    //       id: '',
-    //       avatar: '',
-    //       message: '',
-    //       name: '',
-    //     };
-    //   ]
+      id: randomPostId,
+      url: `photos/${[randomPostId]}.jpg`,
+      description: DESCRIPTIONS[randomPostId - 1],
+      likes: randomPostLikesNumber,
+      comments: commentsList
     };
   } else {
     return createPost();
@@ -113,10 +124,9 @@ const createPost = () => {
 
 // Создаем ленту Кекстограма из постов
 const kekstogramFeed = [];
-while (usedId.length < POSTS_NUMBER) {
+while (postUsedId.length < POSTS_NUMBER) {
   const post = createPost();
   kekstogramFeed.push(post);
 }
 
-console.log(kekstogramFeed);
-
+// console.log(kekstogramFeed);
