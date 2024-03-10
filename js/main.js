4b4e979d7dc310de9e3ac1216c3f6c77c2585bf0
Anastasia <an.gorskova.bg@gmail.com>
2024-03-10 15:19:00 +0100

    убирает константу PhotoId

--- js/main.js
+++ js/main.js
@@ -2,10 +2,10 @@
 // Посты
 const POSTS_NUMBER = 25;
 
-const PhotoId = {
-  MIN: 1,
-  MAX: POSTS_NUMBER
-};
+// const PhotoId = {
+//   MIN: 1,
+//   MAX: POSTS_NUMBER
+// };
 
 const DESCRIPTIONS = [
   'Снимали дроном отель, смотрите какой у нас классный пляж',
